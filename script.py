4bbc5215b0596015160ac45845ed4494d98e7aaa
solarcreature <kothasanju@gmail.com>
2021-09-02 13:37:16 -0600

    Modified to print code

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ import requests;
 
 print(requests.__version__)
 print(requests.get("http://www.google.com"))
-print(requests.get("https://raw.githubusercontent.com/solarcreature/CMPUT_404-Lab_1/master/script.py"))
+print(requests.get("https://raw.githubusercontent.com/solarcreature/CMPUT_404-Lab_1/master/script.py").text)
