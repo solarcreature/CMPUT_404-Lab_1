e93ec901d681fcd577e4fc5a0b4c062a9caaa7e7
solarcreature <kothasanju@gmail.com>
2022-09-12 02:45:13 -0600

    Updated Script

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
 import requests
 
 print(requests.__version__)
+print(requests.get("http://www.google.com/"))
+
+print(requests.get("https://raw.githubusercontent.com/solarcreature/CMPUT_404-Lab_1/main/script.py").text)
